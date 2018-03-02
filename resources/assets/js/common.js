0870ec97ffa1432354b704d8ad61a0e9f58cf3cb
Jeff Yeon <jeff@wbrandstudio.com>
2018-03-02 14:51:27 -0800

    added bb8

--- resources/assets/js/common.js
+++ resources/assets/js/common.js
@@ -185,4 +185,84 @@ moveBackground();
 //
 //
 
+$(document).ready(function() {
+    var newX,
+        currentX = 0,
+        pageWidth = $(this).width(),
+        ball = $('.body'),
+        inner = $('.inner'),
+        head = $('.head'),
+        eye = $('.eye'),
+        antenna = $('.antenna'),
+        mountains = $('.mountains'),
+        sun = $('.sun'),
+        speed = 4,
+        distance,
+        acceleration,
+        antennaPos,
+        idleInterval,
+        started = false;
 
+    $(this).on('mousemove', function(x) {
+        if (!started) {
+            clearInterval(idleInterval);
+            started = true;
+        }
+        newX = x.pageX;
+    });
+
+    $(document).on('touchmove', function(e) {
+        if ("ontouchstart" in document.documentElement) {
+            e = e.originalEvent.touches[0] || e.originalEvent.changedTouches[0];
+        }
+        if (!started) {
+            clearInterval(idleInterval);
+            started = true;
+        }
+        newX = e.pageX;
+    });
+    var idleCount = 1;
+
+    function idle() {
+        newX = pageWidth/2;
+
+        idleInterval =  setInterval(function(){
+            idleCount += speed;
+            newX = pageWidth/2 + (Math.cos(idleCount/100)*100);
+        },10);
+    }
+
+    function movement() {
+        if (currentX !== newX) {
+            distance = newX - currentX;
+            acceleration = Math.abs(distance) / 100;
+            antennaPos = 2250 + Math.round(distance / 5) * 75;
+
+            if (antennaPos <= 0) {
+                antennaPos = 75;
+            } else if (antennaPos >= 4500) {
+                antennaPos = 4500;
+            }
+
+            if (currentX < newX) {
+                currentX += speed * acceleration;
+            } else {
+                currentX -= speed * acceleration;
+            }
+
+            mountains.css('background-position', 300 + -currentX / 50 + 'px 0');
+            sun.css('transform', 'translateX('+  -currentX / 100 + 'px)');
+            inner.css('transform', 'rotateZ(' + currentX / 2 + 'deg)');
+            head.css('transform', 'translateY(-95px) rotateZ(' + distance / 15 + 'deg)');
+            ball.css('transform', 'translateX(' + currentX + 'px)');
+            eye.css('transform', 'translateX(' + distance / 15 + 'px)');
+            antenna.css('background-position', antennaPos + 'px 0');
+
+        } else {
+            return;
+        }
+    }
+
+    setInterval(movement, 10);
+    idle();
+});
--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,12 +1,27 @@
 
 
-<div class="container full-width">
-
-    <div class="row footer-row" >
-
-        <div class="text-center footer-logo animated">
-        <img src="/images/IntelliSkyeWebsiteDarkBlue.png" class="centered img-responsive" alt="footer logo">
+{{--<div class="container full-width">--}}
+    <div class="background">
+        <div class="body">
+            <div class="shadow"></div>
+            <div class="depth"></div>
+            <div class="inner"> </div>
+            <div class="head">
+                <div class="head-shape"></div>
+                <img class="eye" src="http://mike-l.uk/bb-8/img/eye.png" alt="">
+                <div class="antenna"></div>
+            </div>
+        </div>
+        <div class="sun"></div>
+        <div class="sand">
+            <div class="mountains"></div>
         </div>
     </div>
-</div>
+    {{--<div class="row footer-row" >--}}
+
+        {{--<div class="text-center footer-logo animated">--}}
+        {{--<img src="/images/IntelliSkyeWebsiteDarkBlue.png" class="centered img-responsive" alt="footer logo">--}}
+        {{--</div>--}}
+    {{--</div>--}}
+{{--</div>--}}
 
