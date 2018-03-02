06574d79b08897425a9cffcdcf047baeaf3c866b
Jeff Yeon <jeff@wbrandstudio.com>
2018-03-02 14:57:41 -0800

    added bb8

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -8,16 +8,19 @@
             <div class="inner"> </div>
             <div class="head">
                 <div class="head-shape"></div>
-                <img class="eye" src="http://mike-l.uk/bb-8/img/eye.png" alt="">
+                <img class="eye" src="/images/bb8eye.png" alt="">
                 <div class="antenna"></div>
             </div>
         </div>
         <div class="sun"></div>
         <div class="sand">
             <div class="mountains"></div>
-        </div>
-    </div>
-    {{--<div class="row footer-row" >--}}
+            <div class="text-center footer-logo animated">
+<img src="/images/IntelliSkyeWebsiteDarkBlue.png" class="centered img-responsive" alt="footer logo">
+</div>
+</div>
+</div>
+{{--<div class="row footer-row" >--}}
 
         {{--<div class="text-center footer-logo animated">--}}
         {{--<img src="/images/IntelliSkyeWebsiteDarkBlue.png" class="centered img-responsive" alt="footer logo">--}}
