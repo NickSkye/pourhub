52066e784371c2800f9da84cdb4f65816bea75f7
NickSkye <nickskye7@gmail.com>
2018-03-03 22:43:04 -0800

    fixed php

--- public/LookPhp/ConnToServer.php
+++ public/LookPhp/ConnToServer.php
@@ -1,10 +1,10 @@
 <?php
-$connect = @mysql_connect("localhost", "nickskye", "password!");
+$connect = Doctrine\DBAL\Driver\PDOMySql\("localhost", "nickskye", "password!");
 if(!$connect){
 	die('Not connected: ' . mysql_error());
 }
 
-$db_selected = mysql_select_db('look', $connect);
+$db_selected = mssql_select_db('look', $connect);
 if(!$db_selected){
 	die ('Can\'t use foo : ' . mysql_error());
 }
