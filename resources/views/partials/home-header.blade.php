081ec80823453a12e57fdd27d4ac614fa99c6db1
Jeff Yeon <jeff@wbrandstudio.com>
2018-03-15 13:40:42 -0700

    mobile menu

--- resources/views/partials/home-header.blade.php
+++ resources/views/partials/home-header.blade.php
@@ -17,13 +17,13 @@
     	<div class="mobile__nav--container animated slide-in-right">
 			<a href="/" class="brand " ><img src="/images/IntelliSkye_logo_white-blue.png" class="intelliskyelogo" alt=""></a>
     		<ul>
-				<li style="margin-top: 50px;"><a href="/">Home</a></li>
-    			<li><a href="#servs" class="servs">Our Services</a></li>
-    			<li><a href="#teamsm" class="teamsm">Our Team</a></li>
-				<li><a href="#skills" class="skills">Our Skills</a></li>
+				<li style="margin-top: 50px;"><a href="/" style="font-size: 20pt;">Home</a></li>
+    			<li><a href="#servs" class="servs" style="font-size: 20pt;">Our Services</a></li>
+    			<li><a href="#teamsm" class="teamsm" style="font-size: 20pt;">Our Team</a></li>
+				<li><a href="#skills" class="skills" style="font-size: 20pt;">Our Skills</a></li>
     			{{--<li><a href="/about-rejuvenation">About Rejuvenation Wellness</a></li>--}}
 				{{--<li><a href="/faq">FAQ</a></li>--}}
-    			<li><a href="#contact" class="contact">Contact</a></li>
+    			<li><a href="#contact" class="contact" style="font-size: 20pt;">Contact</a></li>
     		</ul>
     	</div>
     </div>
