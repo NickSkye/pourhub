9904b017bc4a0c008561f022b29e8233820fd46b
Jeff Yeon <jeff@wbrandstudio.com>
2018-03-16 15:27:23 -0700

    added carousel

--- resources/views/partials/indexcontent.blade.php
+++ resources/views/partials/indexcontent.blade.php
@@ -1,75 +1,37 @@
-<div class="parallax parazero">
+{{--<div class="parallax parazero">--}}
+   {{--@include('partials.parazerocontent')--}}
+{{--</div>--}}
 
-    <div class="row text-center ">
-        <div class="col-xs-12 top-margin-50">
-            <h1 class="title-load animated ">
-                SIMPLIFYING<br>SOFTWARE
-            </h1>
-            <div class="title-border hidden-xs"></div>
+
+<div id="myCarousel" class="carousel slide" data-ride="carousel" style="z-index: -1; min-height: 900px;">
+
+
+    <div class="carousel-inner">
+
+        <div class="item active ">
+            <div class="parallax parazero">
+            @include('partials.parazerocontent')
+            </div>
         </div>
-    </div>
-    {{--<div class="orangedot"></div>--}}
-    {{--PLANE--}}
-    {{--<div class="frame">--}}
-    {{--<div class="plane-container">--}}
-    {{--<a href="http://intelliskye.com">--}}
-    {{--<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"--}}
-    {{--width="1131.53px" height="379.304px" viewBox="0 0 1131.53 379.304" enable-background="new 0 0 1131.53 379.304"--}}
-    {{--xml:space="preserve" class="plane">--}}
-    {{--<polygon fill="#D8D8D8" points="72.008,0 274.113,140.173 274.113,301.804 390.796,221.102 601.682,367.302 1131.53,0.223  "/>--}}
-    {{--<polygon fill="#C4C4C3" points="1131.53,0.223 274.113,140.173 274.113,301.804 390.796,221.102   "/>--}}
-    {{--</svg></a>--}}
 
-    {{--</div>--}}
-    {{--</div>--}}
-    <div class="clouds" style="top: 8%;">
+        <div class="item">
+            <div class="parallax parazerotwo">
+                @include('partials.parazerotwocontent')
+            </div>
+        </div>
 
-        <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="762px"
-             height="331px" viewBox="0 0 762 331" enable-background="new 0 0 762 331" xml:space="preserve" class="cloud big front slowest">
-<path fill="#FFFFFF" d="M715.394,228h-16.595c0.79-5.219,1.201-10.562,1.201-16c0-58.542-47.458-106-106-106
-c-8.198,0-16.178,0.932-23.841,2.693C548.279,45.434,488.199,0,417.5,0c-84.827,0-154.374,65.401-160.98,148.529
-C245.15,143.684,232.639,141,219.5,141c-49.667,0-90.381,38.315-94.204,87H46.607C20.866,228,0,251.058,0,279.5
-S20.866,331,46.607,331h668.787C741.133,331,762,307.942,762,279.5S741.133,228,715.394,228z"/>
-</svg>
-<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="762px"
-     height="331px" viewBox="0 0 762 331" enable-background="new 0 0 762 331" xml:space="preserve" class="cloud distant smaller">
-<path fill="#FFFFFF" d="M715.394,228h-16.595c0.79-5.219,1.201-10.562,1.201-16c0-58.542-47.458-106-106-106
-c-8.198,0-16.178,0.932-23.841,2.693C548.279,45.434,488.199,0,417.5,0c-84.827,0-154.374,65.401-160.98,148.529
-C245.15,143.684,232.639,141,219.5,141c-49.667,0-90.381,38.315-94.204,87H46.607C20.866,228,0,251.058,0,279.5
-S20.866,331,46.607,331h668.787C741.133,331,762,307.942,762,279.5S741.133,228,715.394,228z"/>
-</svg>
+        <div class="item">
+            <div class="parallax parazerothree">
+                @include('partials.parazerothreecontent')
+            </div>
+        </div>
 
-<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="762px"
-     height="331px" viewBox="0 0 762 331" enable-background="new 0 0 762 331" xml:space="preserve" class="cloud small slow">
-<path fill="#FFFFFF" d="M715.394,228h-16.595c0.79-5.219,1.201-10.562,1.201-16c0-58.542-47.458-106-106-106
-c-8.198,0-16.178,0.932-23.841,2.693C548.279,45.434,488.199,0,417.5,0c-84.827,0-154.374,65.401-160.98,148.529
-C245.15,143.684,232.639,141,219.5,141c-49.667,0-90.381,38.315-94.204,87H46.607C20.866,228,0,251.058,0,279.5
-S20.866,331,46.607,331h668.787C741.133,331,762,307.942,762,279.5S741.133,228,715.394,228z"/>
-</svg>
-
-<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="762px"
-     height="331px" viewBox="0 0 762 331" enable-background="new 0 0 762 331" xml:space="preserve" class="cloud distant super-slow massive">
-<path fill="#FFFFFF" d="M715.394,228h-16.595c0.79-5.219,1.201-10.562,1.201-16c0-58.542-47.458-106-106-106
-c-8.198,0-16.178,0.932-23.841,2.693C548.279,45.434,488.199,0,417.5,0c-84.827,0-154.374,65.401-160.98,148.529
-C245.15,143.684,232.639,141,219.5,141c-49.667,0-90.381,38.315-94.204,87H46.607C20.866,228,0,251.058,0,279.5
-S20.866,331,46.607,331h668.787C741.133,331,762,307.942,762,279.5S741.133,228,715.394,228z"/>
-</svg>
-
-<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="762px"
-     height="331px" viewBox="0 0 762 331" enable-background="new 0 0 762 331" xml:space="preserve" class="cloud slower">
-<path fill="#FFFFFF" d="M715.394,228h-16.595c0.79-5.219,1.201-10.562,1.201-16c0-58.542-47.458-106-106-106
-c-8.198,0-16.178,0.932-23.841,2.693C548.279,45.434,488.199,0,417.5,0c-84.827,0-154.374,65.401-160.98,148.529
-C245.15,143.684,232.639,141,219.5,141c-49.667,0-90.381,38.315-94.204,87H46.607C20.866,228,0,251.058,0,279.5
-S20.866,331,46.607,331h668.787C741.133,331,762,307.942,762,279.5S741.133,228,715.394,228z"/>
-</svg>
 
     </div>
 
-    {{--testend--}}
-    <div id="section05">
-        <a href="#servs" class="servs"><span></span></a>
-    </div>
 </div>
+
+
 {{--<div class="parallax paraone">--}}
 {{--<div>--}}
 {{--<iframe class="animation" id="iframe-animation" src="http://pixijs.teamgbaws.net/header/" style="display: block;">--}}
---
+++ resources/views/partials/parazerotwocontent.blade.php
@@ -0,0 +1,14 @@
+<div class="row text-center ">
+    <div class="col-xs-12 top-margin-50">
+        <h1 class="title-load animated ">
+            ADVANCING<br>THE FUTURE
+        </h1>
+        <div class="title-border hidden-xs"></div>
+    </div>
+</div>
+
+
+    <div id="section05">
+        <a href="#servs" class="servs"><span></span></a>
+    </div>
+</div>
