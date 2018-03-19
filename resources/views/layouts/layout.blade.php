1d2c456cfbe8ce0d604c55678dc5bb143d6895b1
Jeff Yeon <jeff@wbrandstudio.com>
2018-03-19 12:01:14 -0700

    seo

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -40,7 +40,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="icon" href="{{ asset('images/intelliskyeicon.png') }}">
-        <title>IntelliSkye</title>
+        {{--<title>IntelliSkye</title>--}}
         <script src="https://use.fontawesome.com/b36c39fbb0.js"></script>
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
